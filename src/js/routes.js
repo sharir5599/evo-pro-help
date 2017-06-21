0c9b2f7e694a14c12f07437aab85dbe926f29a76
Desmond <desmond@pnw3dev.com>
2017-06-21 11:59:07 -0700

    route cleanup

--- src/js/routes.js
+++ src/js/routes.js
@@ -5,47 +5,15 @@
 // --------------------------------------
 // LANDING
 // --------------------------------------
-route('/', function() {
+function mountLanding() {
     riot.compile(function() {
         riot.mount('#appMount', 'landing-template');
         riot.mount('#globalNavMount', 'landing-template');
     });
+}
+route('/landing', function() {
+    mountLanding();
 });
-// --------------------------------------
-// test
-// --------------------------------------
-// route('/test', function() {
-//     riot.compile(function() {
-//         riot.mount('#appMount', 'app-global-nav');
-//         riot.mount('#globalNavMount', 'auth-transition-template');
-//     });
-// });
-// // --------------------------------------
-// GUIDE HOME !!! DEFAULT ROUTE !!!
-// --------------------------------------
-// route('/', function() {
-//     riot.compile(function() {
-//         // -----------------------------------
-//         // SUBDOMAIN HANDLER
-//         // -----------------------------------
-//         var hostname = window.location.hostname.split('.com')[0];
-//         if (hostname.includes('.')) {
-//             RC.trigger('UM_GET_COMPANY');
-//             // SUBDOMAIN APP REDIRECT
-//             if (evoAuth.currentUser) {
-//                 // riot.mount('#appMount', 'app-global-nav');
-//                 riot.mount('#globalNavMount', 'app-module-nav');
-//                 riot.mount('#moduleNavMount', 'dashboard-company-template');
-//             } else {
-//                 // riot.mount('#appMount', 'app-global-nav');
-//                 riot.mount('#globalNavMount', 'login-template');
-//                 riot.mount('#moduleNavMount', 'login-template');
-//             }
-//             RC.trigger('LOG_DEBUG', 'Subdomain Detected!', 'routes');
-//         } else {
-//             // DEFAULT APP REDIRECT
-//             riot.mount('#appMount', 'landing-template');
-//             riot.mount('#globalNavMount', 'landing-template');
-//         }
-//     });
-// });
+route('/', function() {
+    mountLanding();
+});
